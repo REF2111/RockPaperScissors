7ec0db2d8b5adabe1b13f218c588d227cd823f23
Raphael Berendes <raphael-berendes@gmx.de>
2019-04-08 10:28:51 +0200

    Add game model

---
+++ RockPaperScissors/RockPaperScissors/GameModel.swift
@@ -0,0 +1,51 @@
+//
+//  GameModel.swift
+//  RockPaperScissors
+//
+//  Created by Raphael Berendes on 08.04.19.
+//  Copyright © 2019 Raphael Berendes. All rights reserved.
+//
+
+import Foundation
+
+enum Tool: Int {
+    case rock
+    case paper
+    case scissors
+}
+
+enum GameResult: String {
+    case win = "Win"
+    case loss = "Loss"
+    case draw = "Draw"
+}
+
+struct Game {
+    
+    let playerTool: Tool
+    let computerTool: Tool
+    
+    var result: GameResult {
+        switch playerTool {
+        case .rock:
+            switch computerTool {
+            case .rock: return .draw
+            case .paper: return .loss
+            case .scissors: return .win
+            }
+        case .paper:
+            switch computerTool {
+            case .rock: return .win
+            case .paper: return .draw
+            case .scissors: return .loss
+            }
+        case .scissors:
+            switch computerTool {
+            case .rock: return .loss
+            case .paper: return .win
+            case .scissors: return .draw
+            }
+        }
+    }
+}
+
