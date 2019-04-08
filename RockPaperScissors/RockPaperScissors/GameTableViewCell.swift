5f5ada190bc0275e4bcb8b7c1e68f96e313935ff
Raphael Berendes <raphael-berendes@gmx.de>
2019-04-08 10:43:43 +0200

    Add table view cell for tools

---
+++ RockPaperScissors/RockPaperScissors/GameTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  GameTableViewCell.swift
+//  RockPaperScissors
+//
+//  Created by Raphael Berendes on 08.04.19.
+//  Copyright © 2019 Raphael Berendes. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var toolImageView: UIImageView!
+    
+    func setImage(for tool: Tool) {
+        
+        var toolImage: UIImage?
+        
+        switch tool {
+        case .rock:
+            toolImage = UIImage(named: "rock")
+        case .paper:
+            toolImage = UIImage(named: "paper")
+        case .scissors:
+            toolImage = UIImage(named: "scissors")
+        }
+        
+        guard let image = toolImage else { return }
+        toolImageView.image = image
+    }
+    
+}
